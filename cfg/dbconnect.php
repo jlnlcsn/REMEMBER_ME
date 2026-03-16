b10185429a550fa7c1cae3381b709e726373b49d
jlnlcsn <julianalacsina@gmail.com>
2026-03-16 23:22:03 +0800

    Fix formatting in dbconnect.php

--- cfg/dbconnect.php
+++ cfg/dbconnect.php
@@ -3,7 +3,7 @@
   $userid="root";
   $pwd="";
   $dbname="users_db";
-  $conn = mysqli_connect($server, $userid, $pwd, $dbname);
+  $conn = mysqli_connect("localhost", "root", "", "users_db");
 //Check connection
 if (!$conn) 
   	die("Connection Error: " . mysqli_connect_error());
